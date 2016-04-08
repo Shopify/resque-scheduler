46f2bfb72166bbdd913cff4829b455de0d444d0b
Hormoz K <hkheradm@gmail.com>
2016-04-08 14:33:25 -0400

    use SCRIPT to set schedule and deprecate old method

--- test/multi_process_test.rb
+++ test/multi_process_test.rb
@@ -3,35 +3,78 @@ require_relative 'test_helper'
 
 context 'Multi Process' do
   test 'setting schedule= from many process does not corrupt the schedules' do
-    schedules = {}
-    counts  = []
-    threads = []
+    schedules_1 = {}
+    schedules_2 = {}
+    counts = []
+    pids = []
 
     # This number may need to be increased if this test is not failing
-    processes = 20
+    processes = 100
 
-    schedule_count = 200
+    schedule_count = 300
 
     schedule_count.times do |n|
-      schedules["job #{n}"] = { cron: '0 1 0 0 0' }
+      schedules_1["1_job_#{n}"] = { cron: '0 1 0 0 0' }
+      schedules_2["2_job_#{n}"] = { cron: '0 1 0 0 0' }
     end
 
     processes.times do |n|
-      threads << Thread.new do
+      pids << fork_with_marshalled_pipe_and_result do
         sleep n * 0.1
-        Resque.schedule = schedules
-        counts << Resque.schedule.size
+        Resque.schedule = n.even? ? schedules_2 : schedules_1
+        Resque.schedule.size
       end
     end
 
-    # doing this outside the threads increases the odds of failure
-    Resque.schedule = schedules
-    counts << Resque.schedule.size
-
-    threads.each(&:join)
+    counts += get_results_from_children(pids)
 
     counts.each_with_index do |c, i|
       assert_equal schedule_count, c, "schedule count is incorrect (c: #{i})"
     end
+  end
+
+  private
+
+  def fork_with_marshalled_pipe_and_result
+    pipe_read, pipe_write = IO.pipe
+    pid = fork do
+      pipe_read.close
+      result = begin
+        [yield, nil]
+      rescue StandardError => exc
+        [nil, exc]
+      end
+      pipe_write.syswrite(Marshal.dump(result))
+      # exit true the process to get around fork issues on minitest 5
+      # see https://github.com/seattlerb/minitest/issues/467
+      Process.exit!(true)
+    end
+    pipe_write.close
+
+    [pid, pipe_read]
+  end
+
+  def get_results_from_children(children)
+    results = []
+    children.each do |pid, pipe|
+      wait_for_child_process_to_terminate(pid)
+
+      fail "forked process failed with #{$CHILD_STATUS}" unless $CHILD_STATUS.success?
+      result, exc = Marshal.load(pipe.read)
+      fail exc if exc
+      results << result
+    end
+    results
+  end
+
+  def wait_for_child_process_to_terminate(pid = -1, timeout: 30)
+    Timeout.timeout(timeout) do
+      Process.wait(pid)
+    end
+  rescue Timeout::Error
+    Process.kill('KILL', pid)
+    # collect status so it doesn't stick around as zombie process
+    Process.wait(pid)
+    flunk 'Child process did not terminate in time.'
   end
 end
