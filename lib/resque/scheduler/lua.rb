de389f6b5ae289cedc6e9677f38381a6184df4a9
Hormoz K <hkheradm@gmail.com>
2018-02-28 10:04:57 -0500

    cleanup the lua module

--- lib/resque/scheduler/lua.rb
+++ lib/resque/scheduler/lua.rb
@@ -5,24 +5,20 @@ module Resque
 
       LUA_SCRIPTS_PATH = File.join(File.dirname(__FILE__), 'lua')
 
-      def self.zpop(key, *args)
-        evalsha(:zpop, [key], args)
+      def zpop(key, min, max, offset, count)
+        evalsha(:zpop, [key], [min, max, offset, count])
       end
 
       private
 
-      def self.zpop_sha(refresh = false)
+      def zpop_sha(refresh = false)
         @zpop_sha = nil if refresh
         @zpop_sha ||= Resque.redis.script(:load, load_lua("zpop"))
       end
 
-      def self.evalsha(script, keys, argv, refresh: false)
+      def evalsha(script, keys, argv, refresh: false)
         sha_method_name = "#{script}_sha"
-        Resque.redis.evalsha(
-          send(sha_method_name, refresh),
-          keys: keys,
-          argv: argv
-        )
+        Resque.redis.evalsha(send(sha_method_name, refresh), keys: keys, argv: argv)
       rescue Redis::CommandError => e
         if e.message =~ /NOSCRIPT/
           refresh = true
